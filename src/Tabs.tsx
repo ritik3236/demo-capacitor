fe697084b85f04ff25a111e085561bc1ffd64c76
Ritik <Ritikkushwaha1234@gmail.com>
2024-05-11 11:03:48 +0530

    Added Support for API

--- src/Tabs.tsx
+++ src/Tabs.tsx
@@ -4,42 +4,35 @@ import { Redirect, Route } from 'react-router';
 
 import { IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
 
-import { library, playCircle, radio, search } from 'ionicons/icons';
+import { home, library, receipt } from 'ionicons/icons';
 
-import { CameraPage, DeviceInfo, Home, HttpsPage, PushNotificationPage } from 'src/pages';
+import { CameraPage, Home, HttpsPage } from 'src/pages';
 
 export const Tabs: React.FC = () => {
     return (
         <IonTabs>
             <IonRouterOutlet>
                 <Route path="/app/home" render={() => <Home/>} exact={true}/>
-                <Route path="/app/https" render={() => <HttpsPage/>} exact={true}/>
-                <Route path="/app/camera" render={() => <CameraPage/>} exact={true}/>
-                <Route path="/app/push-notification" render={() => <PushNotificationPage/>} exact={true}/>
-                <Route path="/app/device-info" render={() => <DeviceInfo/>} exact={true}/>
+                <Route path="/app/orders" render={() => <HttpsPage/>} exact={true}/>
+                <Route path="/app/customers" render={() => <CameraPage/>} exact={true}/>
                 <Route exact path="/app">
                     <Redirect to="/app/home"/>
                 </Route>
             </IonRouterOutlet>
             <IonTabBar slot="bottom">
                 <IonTabButton tab="home" href="/app/home">
-                    <IonIcon icon={playCircle}/>
-                    <IonLabel>Listen now</IonLabel>
+                    <IonIcon icon={home}/>
+                    <IonLabel>Home</IonLabel>
                 </IonTabButton>
 
-                <IonTabButton tab="https" href="/app/https">
-                    <IonIcon icon={radio}/>
-                    <IonLabel>Radio</IonLabel>
+                <IonTabButton tab="orders" href="/app/orders">
+                    <IonIcon icon={receipt}/>
+                    <IonLabel>Orders</IonLabel>
                 </IonTabButton>
 
-                <IonTabButton tab="camera" href="/app/camera">
+                <IonTabButton tab="customers" href="/app/customers">
                     <IonIcon icon={library}/>
-                    <IonLabel>Library</IonLabel>
-                </IonTabButton>
-
-                <IonTabButton tab="device-info" href="/app/device-info">
-                    <IonIcon icon={search}/>
-                    <IonLabel>Search</IonLabel>
+                    <IonLabel>Customers</IonLabel>
                 </IonTabButton>
             </IonTabBar>
         </IonTabs>
